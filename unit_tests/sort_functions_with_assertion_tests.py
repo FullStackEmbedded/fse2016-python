c2b53774669b8ae5e2b26f989f4ac385c7300473
Daniel Lee <erget2005@gmail.com>
2016-10-29 17:09:08 +0200

    Prettify self-contained sort module with unit tests

--- unit_tests/sort_functions_with_assertion_tests.py
+++ unit_tests/sort_functions_with_assertion_tests.py
@@ -49,11 +49,11 @@ if __name__ == "__main__":
     print my_function.__doc__
     my_ints = [1, 2, 3, 4, 5]
     my_strings = ["a", "b", "c", "d"]
-    assert [5, 4, 3, 2, 1] == reverse(my_ints) == reverse_recursive(my_ints)
-    assert (["d", "c", "b", "a"] == reverse(my_strings) ==
-            reverse_recursive(my_strings))
+    assert([5, 4, 3, 2, 1] == reverse(my_ints) == reverse_recursive(my_ints))
+    assert(["d", "c", "b", "a"] == reverse(my_strings) ==
+           reverse_recursive(my_strings))
     other_strings = ["d", "a", "l"]
     other_ints = [10, 5, 0]
-    assert (["a", "d", "l"] == sort(other_strings) ==
-            sort_recursive(other_strings))
-    assert [0, 5, 10] == sort(other_ints) == sort_recursive(other_ints)
+    assert(["a", "d", "l"] == sort(other_strings) ==
+           sort_recursive(other_strings))
+    assert([0, 5, 10] == sort(other_ints) == sort_recursive(other_ints))
